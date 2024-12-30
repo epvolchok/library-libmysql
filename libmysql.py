23f1ea8edf6c379a2278d8af654bd41faec1b5f4
epvolchok <131437390+epvolchok@users.noreply.github.com>
2024-12-31 00:09:41 +0100

    Added license info to the lib file

--- libmysql.py
+++ libmysql.py
@@ -1,3 +1,11 @@
+#Copyright (c) 2024 VOLCHOK Evgeniia
+#for contacts e.p.volchok@gmail.com
+
+#Licensed under the Apache License, Version 2.0 (the "License");
+#you may not use this file except in compliance with the License.
+#You may obtain a copy of the License at
+#http://www.apache.org/licenses/LICENSE-2.0
+
 from getpass import getpass
 from mysql.connector import connect, Error
 import pandas as pd
